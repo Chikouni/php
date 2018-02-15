5a5bc6745bd65c9417331230f575146568d347e0
Chikouni <louisdoubi@gmail.com>
2018-02-15 15:36:03 +0100

    Add files via upload

---
+++ PHP_POO-Incomplet/Config.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Description of Config
+ *
+ * @author Alan
+ */
+class Config {
+    const SERVEUR = "latetedarwepsi.mysql.db";
+    const BASE = "latetedarwepsi";
+    const UTILISATEUR = "latetedarwepsi";
+    const MOTDEPASSE = "EpsiEpsi44";
+}
