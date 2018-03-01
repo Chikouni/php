8896b6fc08588e5580c82367eef6c7cf399f88db
AxelBlusseau <36446232+AxelBlusseau@users.noreply.github.com>
2018-03-01 11:15:57 +0100

    Create menu.php
    C'est juste un menu test pour tester la sécurité des données d'une session.

---
+++ menu.php
@@ -0,0 +1,50 @@
+<?php
+
+session_start();
+
+include_once 'Config.php';
+
+$db = new PDO("mysql:host=" . Config::SERVEUR . ";dbname=" . Config::BASE
+, Config::UTILISATEUR, Config::MOTDEPASSE); 
+
+if(isset($_GET['id'])) 
+{
+    $getid = $_GET['id'];
+    $requser = $db->prepare("SELECT * FROM utilisateur WHERE identifiant = :identifiant");
+    $requser->execute([":identifiant" => $getid]);
+    
+    $userinfo = $requser->fetch();
+
+?>
+
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Menu</title>
+    </head>
+    <body>
+        <h2>Menu de <?php echo $userinfo['prenom']; ?></h2>
+        <br /><br />
+        Identifiant = <?php echo $userinfo['identifiant']; ?>
+        <br />
+        Nom = <?php echo $userinfo['nom']; ?>
+
+        <?php 
+        if($userinfo['identifiant'] == $_SESSION['id'])
+        {
+        ?>
+        <a href="deconnexion.php">Deconnexion</a>
+        <?php
+        }
+        ?>
+        
+        
+    </body>
+</html>
+
+
+<?php
+}       //Si l'id existe ça affiche la page     
+?>
