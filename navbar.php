a9667fb6bd605a8a26ef71a055bfd1b9889e4b74
AxelBlusseau <36446232+AxelBlusseau@users.noreply.github.com>
2018-03-25 16:57:40 +0200

    Create navbar.php

---
+++ navbar.php
@@ -0,0 +1,26 @@
+        <nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
+            <a class="navbar-brand" href="#"><img src="img/logo-m.png" width="50" height="50" class="d-inline-block align-top"></a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarsExampleDefault">
+                <ul class="navbar-nav mr-auto">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="menu.php">Accueil</a><span class="sr-only">(current)</span>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="" id="dropdown01" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Menu</a>
+                        <div class="dropdown-menu" aria-labelledby="dropdown01">
+                            <a class="dropdown-item" href="meschatrooms.php">Mes Chatrooms</a>
+                            <a class="dropdown-item" href="autreschatrooms.php">Autres Chatrooms</a>
+                            <a class="dropdown-item" href="invitations.php">Invitations</a>
+                            <a class="dropdown-item" href="deconnexion.php">Deconnexion</a>
+                        </div>
+                    </li>
+                </ul>
+                <form class="form-inline my-2 my-lg-0">
+                    <input class="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search">
+                    <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Rechercher</button>
+                </form>
+            </div>
+        </nav>
