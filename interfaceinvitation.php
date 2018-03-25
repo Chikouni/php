6787a2720822199dfa791720baaebe6f6892ae96
AxelBlusseau <36446232+AxelBlusseau@users.noreply.github.com>
2018-03-25 19:07:49 +0200

    Create interfaceinvitation.php

---
+++ interfaceinvitation.php
@@ -0,0 +1,76 @@
+<?php
+include_once 'navbar.php';
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <link href="invitation.css" rel="stylesheet" type="text/css"/>
+        <title>Invitations</title>
+    </head>
+    <body>
+        <div class="container">
+            <div class="menu ">
+
+                <h1>Invitations</h1><br/>
+
+                <div class="liste">
+                    <div class="btn-group-vertical">
+
+                        <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#myModal">Invitation de "nom d'user"</button>
+                        <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#myModal">Invitation de "nom d'user"</button>
+                        <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#myModal">Invitation de "nom d'user"</button>
+                        <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#myModal">Invitation de "nom d'user"</button>
+                        <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#myModal">Invitation de "nom d'user"</button>
+                        <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#myModal">Invitation de "nom d'user"</button>
+                        <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#myModal">Invitation de "nom d'user"</button>
+                        <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#myModal">Invitation de "nom d'user"</button>
+                        <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#myModal">Invitation de "nom d'user"</button>
+                        <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#myModal">Invitation de "nom d'user"</button>
+
+                    </div>
+                </div>
+
+                <!-- The Modal -->
+                <div class="modal fade" id="myModal">
+                    <div class="modal-dialog">
+                        <div class="modal-content">
+
+                            <!-- Modal Header -->
+                            <div class="modal-header">
+                                <h4 class="modal-title">Option invitation</h4>
+                                <button type="button" class="close" data-dismiss="modal">&times;</button>
+                            </div>
+
+                            <!-- Modal body -->
+                            <div class="modal-body">
+                               
+                                <a href="#" class="btn btn-success" type="button">Accepter</a>
+                                <a href="#" class="btn btn-success" type="button">Refuser</a>
+            
+                            </div>
+
+                            <!-- Modal footer -->
+                            <div class="modal-footer">
+                                <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </body>
+    <footer>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+        <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+    </footer>
+</html>
