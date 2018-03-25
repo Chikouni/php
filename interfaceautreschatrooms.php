19fe21bdfb329398eec5c13b398c0b7fc0403528
AxelBlusseau <36446232+AxelBlusseau@users.noreply.github.com>
2018-03-25 19:14:09 +0200

    Create interfaceautreschatrooms.php

---
+++ interfaceautreschatrooms.php
@@ -0,0 +1,44 @@
+<?php
+include_once 'navbar.php';
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <link href="meschatrooms.css" rel="stylesheet" type="text/css"/>
+        <title>Mes Chatrooms</title>
+    </head>
+    <body>
+        <div class="container">
+            <div class="menu ">
+
+                <h1>Mes Chatrooms</h1><br/>
+
+                <div class="liste">
+                    <div class="btn-group-vertical">
+                    <a href="#" class="btn btn-success" type="button">Chatroom 1</a>
+                    <a href="#" class="btn btn-success" type="button">Chatroom 2</a>
+                    <a href="#" class="btn btn-success" type="button">Chatroom 3</a>
+                    <a href="#" class="btn btn-success" type="button">Chatroom 4</a>
+                    <a href="#" class="btn btn-success" type="button">Chatroom 5</a>
+                    <a href="#" class="btn btn-success" type="button">Chatroom 6</a>
+                    <a href="#" class="btn btn-success" type="button">Chatroom 7</a>
+                    <a href="#" class="btn btn-success" type="button">Chatroom 8</a>
+                    <a href="#" class="btn btn-success" type="button">Chatroom 9</a>
+                    <a href="#" class="btn btn-success" type="button">Chatroom 10</a>
+                    <a href="#" class="btn btn-success" type="button">Chatroom 11</a>
+                    <a href="#" class="btn btn-success" type="button">Chatroom 12</a>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </body>
+    <footer>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+        <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    </footer>
+</html>
